8b7f783975546bf28f1ee57894823df35dcd6b98
BRUNO NAHUEL MARASI <48113202@ort>
2025-06-13 10:14:24 -0300

    terminamos2

--- react/src/Views/Productos/Productos.jsx
+++ react/src/Views/Productos/Productos.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ProductosCard from "../../Componentes/ProductosCard/ProductosCard"
 import "./Productos.css"
